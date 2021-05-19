37199f6d4164ef3b0bc289f5d1e6e6a4d59b03b5
tushal012 <tushalkashyap11@gmail.com>
2021-05-19 16:33:50 +0530

    Welcome to Employee Wage Computation

---
+++ src/main/java/combridgelabz/employeeWageBuilder.java
@@ -0,0 +1,7 @@
+package combridgelabz;
+
+public class employeeWageBuilder {
+    public static void main(String[] args) {
+        System.out.println("Welcome to Employee Wage Computation");
+    }
+}
